f00c97009851b4f62ee8e69871641d70d6bc4fc0
Atiqur Rahman <me@atiqur.xyz>
2024-12-01 17:00:24 +0600

    init files

---
+++ sample/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  
+  styleUrl: './app.component.css',
+  template: `
+    <h1>Default</h1>
+  `,
+})
+export class AppComponent {
+  title = 'homes';
+}
